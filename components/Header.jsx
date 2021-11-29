8b4cfb6011d4e1a1e54a02b92912aacd555e468f
drnareshchauhan <drnareshchauhan@gmail.com>
2021-11-29 16:35:20 +0530

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+import Link from "next/link";
+import { getCategories } from "../services";
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl text-white">
+              <h1 className="lg:float-left">
+                pSm
+                <img src="https://img.icons8.com/external-becris-lineal-color-becris/70/000000/external-review-customer-loyalty-program-becris-lineal-color-becris.png" />
+              </h1>
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
